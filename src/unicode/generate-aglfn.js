3f2559b06da86e94ad79762c065893909b7d2d4f
Bartosz Lorek <bery.lorek@gmail.com>
2017-12-14 22:19:37 +0100

    lookup table of aglfn specification

--- src/unicode/generate-aglfn.js
+++ src/unicode/generate-aglfn.js
@@ -1,30 +1,41 @@
 const fileParser = require('../.utils/file-parser')
+const unidata = require('./lookup-table/unidata')
+const catdata = require('./lookup-table/categories')
 const unicodeRange = require('unicode-range')
 const path = require('path')
 
+const toDescription = name => catdata[name]
+
 fileParser({
     source: path.join(__dirname, './resources/aglfn.txt'),
-    output: path.join(__dirname, 'table.js'),
+    output: path.join(__dirname, './lookup-table/aglfn.js'),
     iteratee: (line, props) => {
         let fields = line.split(';'),
-            block = unicodeRange(fields[0])
+            block = unicodeRange(fields[0]),
+            { category } = unidata[fields[0]]
+
         return JSON.stringify({
             value: fields[0],
             name: fields[2].toLowerCase(),
             symbol: escapeSymbol(fields[0]),
-            block: blockIndex(props.blocks, block)
+            category: getIndex(props.categories, category),
+            block: getIndex(props.blocks, block)
         })
     },
     props: {
+        categories: [],
         blocks: []
     },
     separator: ',',
     before: 'module.exports={glyphs:[',
-    after: (props) => '], blocks:' +
-        JSON.stringify(props.blocks) + '}'
+    after: (props) => {
+        let c = JSON.stringify(props.categories.map(toDescription)),
+            b = JSON.stringify(props.blocks)
+        return `],categories:${c},blocks:${b}}`
+    }
 })
 
-function blockIndex(array, name) {
+function getIndex(array, name) {
     let index = array.indexOf(name)
     if (index > -1) {
         return index
