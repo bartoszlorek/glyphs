3eddefe219b9e8a123028dca1bdf26bdf3259b3e
Bartosz Lorek <bery.lorek@gmail.com>
2017-12-21 00:56:13 +0100

    handle search

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import table from '../unicode/lookup-table/aglfn'
+import { bind } from '../.utils/react-utils'
 
 import Glyph from './Glyph'
 import style from './style.css'
 
 class App extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
+        bind(this, [
+            'handleSearch'
+        ])
         this.state = {}
+    }
+
+    handleSearch(e) {
+        console.log(e.target.value)
     }
 
     render() {
         return (
             <div>
-                <input type='text' />
+                <input
+                    type='text'
+                    onChange={this.handleSearch}
+                />
                 <div className={style.container}>
                     {table.glyphs.map((data, index) =>
                         <Glyph key={index} data={data} />
--- src/components/Glyph.js
+++ src/components/Glyph.js
@@ -4,7 +4,10 @@ import style from './style.css'
 function Glyph(props) {
     let { value, name, symbol } = props.data
     return (
-        <div className={style.glyph}>
+        <div
+            className={style.glyph}
+            data-name={name}
+        >
             <span className={style.symbol}>{symbol}</span>
             <span className={style.value}>{value}</span>
         </div>
