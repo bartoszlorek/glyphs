2ceb010c667109f7d925ef5c120524ff63087d7a
Bartosz Lorek <bery.lorek@gmail.com>
2018-02-16 01:09:52 +0100

    counts and limits execution of code for given tab

---
+++ src/.utils/chrome/execution-counter.js
@@ -0,0 +1,55 @@
+import { isPlainObject } from 'lodash'
+
+const RESET_COUNTER = 'RESET_COUNTER'
+
+function bindEmitter() {
+    window.addEventListener('beforeunload', () => {
+        chrome.runtime.sendMessage({ type: RESET_COUNTER })
+    })
+}
+
+function bindListener(cache) {
+    chrome.runtime.onMessage.addListener((request, sender) => {
+        if (request.type === RESET_COUNTER) {
+            cache[sender.tab.id] = -1
+        }
+    })
+    return cache
+}
+
+function createCounter(cache) {
+    if (!isPlainObject(cache)) {
+        throw 'createCounter requires `cache` as an Object.'
+    }
+
+    return name => {
+        const value = cache[name] + 1 || 0
+        cache[name] = value
+
+        const api = {
+            reset: () => {
+                cache[name] = -1
+                return api
+            },
+            equal: (x, callback) => {
+                if (value === x) {
+                    callback()
+                }
+                return api
+            },
+            greater: (x, callback) => {
+                if (value > x) {
+                    callback()
+                }
+                return api
+            }
+        }
+        return api
+    }
+}
+
+export {
+    createCounter,
+    bindEmitter,
+    bindListener
+}
