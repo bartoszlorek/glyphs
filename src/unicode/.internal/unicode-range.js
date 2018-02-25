19f828e9e69df462d0c052ec08375e31450cfee8
Bartosz Lorek <bery.lorek@gmail.com>
2018-02-26 00:57:01 +0100

    returns block name and index for given unicode value

---
+++ src/unicode/.internal/unicode-range.js
@@ -0,0 +1,25 @@
+const ranges = require('unicode-range-json')
+
+module.exports = function(unicodestr) {
+    if (typeof unicodestr !== 'string') {
+        throw new TypeError('unicode-range expected a String')
+    }
+
+    let value = parseInt(unicodestr.replace(/^U\+/i, ''), 16)
+
+    for (let index = 0; index < ranges.length; index++) {
+        let range = ranges[index]
+
+        if (value >= range.range[0] && value <= range.range[1]) {
+            return {
+                name: range.category,
+                index
+            }
+        }
+    }
+
+    return {
+        name: 'Unassigned',
+        index: 0
+    }
+}
