99431223bdb97f7bb7a739ad712592851019ed4b
Bartosz Lorek <bery.lorek@gmail.com>
2018-02-28 00:22:56 +0100

    we can use react-select instead of plus

--- src/popup/components/Select.jsx
+++ src/popup/components/Select.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import SelectPlus from 'react-select-plus'
 
-import '../select-plus-style/select.less'
+import '../select-style/select.less'
 
 function Select(props) {
     return <SelectPlus {...props} multi={true} searchable={false} />
