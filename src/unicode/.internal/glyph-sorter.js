c772393235c4c84d434ec2a7a4cc13bdaa5086ed
Bartosz Lorek <bery.lorek@gmail.com>
2018-02-28 13:52:00 +0100

    move require inside function

--- src/unicode/.internal/glyph-sorter.js
+++ src/unicode/.internal/glyph-sorter.js
@@ -1,15 +1,20 @@
 const fs = require('fs')
+const path = require('path')
 const hex = require('./esc-hexadecimal')
 
 const sortValues = (a, b) => hex(a.value) - hex(b.value)
 
-function glyphSorter({ data, file }) {
-    let sorted = data.glyphs.sort(sortValues),
-        glyphs = JSON.stringify(sorted),
-        groups = JSON.stringify(data.groups)
+function glyphSorter({ source, output }) {
+    if (path.extname(source) !== '.js') {
+        throw new Error(`source (${source}) must be a JavaScript file`)
+    }
 
-    let output = `module.exports={glyphs:${glyphs},groups:${groups}}`
-    fs.writeFile(file, output, 'utf8', () =>
+    let { glyphs, groups } = require(source),
+        strGlyphs = JSON.stringify(glyphs.sort(sortValues)),
+        strGroups = JSON.stringify(groups)
+
+    let data = `module.exports={glyphs:${strGlyphs},groups:${strGroups}}`
+    fs.writeFile(output, data, 'utf8', () =>
         console.log('glyphs sorted successfully')
     )
 }
--- src/unicode/sort-aglfn-gg.js
+++ src/unicode/sort-aglfn-gg.js
@@ -2,6 +2,6 @@ const path = require('path')
 const sorter = require('./.internal/glyph-sorter')
 
 sorter({
-    file: path.join(__dirname, './lookup-tables/aglfn-gg-sorted.js'),
-    data: require('./lookup-tables/aglfn-gg')
+    source: path.join(__dirname, './lookup-tables/aglfn-gg.js'),
+    output: path.join(__dirname, './lookup-tables/aglfn-gg-sorted.js')
 })
