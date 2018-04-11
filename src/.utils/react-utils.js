94d7ed997d23bad29ec1f969eb1679df57d1af5d
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-11 12:33:48 +0200

    update utils

--- src/.utils/react-utils.js
+++ src/.utils/react-utils.js
@@ -1,7 +1,36 @@
-export { bind }
+import { get, isPlainObject } from 'lodash'
 
-function bind(context, methods) {
+export function choose(path, cases) {
+    if (isPlainObject(cases)) {
+        return object => {
+            let value = get(object, path)
+            return cases[String(value)]
+        }
+    }
+    return object => {
+        let value = get(object, path)
+        return value && cases
+    }
+}
+
+export function bind(context, methods) {
     methods.forEach(item => {
         context[item] = context[item].bind(context)
     })
+}
+
+export function omit(object, props) {
+    if (object == null) {
+        return null
+    }
+    if (props == null || !props.length) {
+        return object
+    }
+    let newObject = {}
+    Object.keys(object).forEach(prop => {
+        if (props.indexOf(prop) < 0) {
+            newObject[prop] = object[prop]
+        }
+    })
+    return newObject
 }
