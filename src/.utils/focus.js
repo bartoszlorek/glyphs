96594ae5ab978dee9bf389a6e761b0c0eb253286
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-11 21:55:15 +0200

    redefines focus api

--- src/.utils/focus.js
+++ src/.utils/focus.js
@@ -5,55 +5,71 @@ function createFocus(limit = 10) {
         return elem === history[history.length - 1]
     }
 
-    const pushToHistory = elem => {
+    const pushHistory = elem => {
         if (elem == null || isLast(elem)) {
             return
         }
         if (history.length === limit) {
             history.shift()
         }
+        if (self.logger !== null) {
+            self.logger(elem, history)
+        }
         history.push(elem)
     }
 
     const handleClick = () => {
-        pushToHistory(document.activeElement)
+        pushHistory(document.activeElement)
     }
 
-    const self = {
-        push: elem => {
-            if (elem == 'active') {
-                elem = document.activeElement
-            }
-            pushToHistory(elem)
+    const self = (index = -1, predicate) => {
+        let length = history.length
+        if (!length) {
             return self
-        },
-        attach: () => {
-            document.addEventListener('click', handleClick, true)
+        }
+
+        if (typeof index === 'function') {
+            index = index(history)
+        }
+        if (index < 0) {
+            index = -index > length ? 0 : length + index
+        } else if (index >= length) {
+            index = length - 1
+        }
+        if (index == null) {
             return self
-        },
-        detach: () => {
-            document.removeEventListener('click', handleClick, true)
-            return self
-        },
-        prev: (value = 0, predicate) => {
-            let index = value == null ? -1 : history.length - value
-            if (index < 0) {
+        }
+
+        let elem = history[index]
+        if (typeof predicate === 'function') {
+            let result = predicate(elem)
+            if (result === false) {
                 return self
             }
-            let elem = history[index]
-            if (typeof predicate === 'function') {
-                let result = predicate(elem)
-                if (result === false) {
-                    return self
-                }
-                if (result && result.nodeType === 1) {
-                    elem = result
-                }
+            if (result && result.nodeType === 1) {
+                elem = result
             }
-            elem.focus()
-            return self
-        },
-        last: predicate => self.prev(0, predicate)
+        }
+
+        elem.focus()
+        return self
+    }
+
+    self.logger = null
+    self.attach = () => {
+        document.addEventListener('click', handleClick, true)
+        return self
+    }
+    self.detach = () => {
+        document.removeEventListener('click', handleClick, true)
+        return self
+    }
+    self.push = elem => {
+        if (elem === 'active') {
+            elem = document.activeElement
+        }
+        pushHistory(elem)
+        return self
     }
 
     return self
